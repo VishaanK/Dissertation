0a553acd330a98bb9f6640014b14425b3789c822
vishaanK <vishaan.khanna@googlemail.com>
2024-11-12 18:50:31 +0000

    tested chaincode

--- testNet/fabric-network/scripts/testChaincode.sh
+++ testNet/fabric-network/scripts/testChaincode.sh
@@ -14,7 +14,7 @@ export CORE_PEER_MSPCONFIGPATH=../organizations/peerOrganizations/org1.example.c
 export ORDERER_GENERAL_TLS_ROOTCAS=../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt
 
 #create documents and then check they exist
-../bin/peer chaincode invoke --ctor '{"Function":"CheckDocumentExists", "Args":["doc1"]}' -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+../bin/peer chaincode invoke --ctor '{"Function":"InitLedger", "Args":[]}' -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
     --channelID channel1 --name document \
     --peerAddresses localhost:7051 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
     --peerAddresses localhost:7060 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" \
@@ -22,9 +22,9 @@ export ORDERER_GENERAL_TLS_ROOTCAS=../organizations/ordererOrganizations/example
     --tls --cafile $ORDERER_GENERAL_TLS_ROOTCAS
 
 
-# ../bin/peer chaincode invoke --ctor '{"Function":"CheckDocumentExists", "Args":["doc1"]}' -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
-#     --channelID channel1 --name document \
-#     --peerAddresses localhost:7051 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
-#     --peerAddresses localhost:7060 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" \
-#     --peerAddresses localhost:7057 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt" \
-#     --tls --cafile $ORDERER_GENERAL_TLS_ROOTCAS
+ ../bin/peer chaincode invoke --ctor '{"Function":"CheckDocumentExists", "Args":["doc1"]}' -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+     --channelID channel1 --name document \
+     --peerAddresses localhost:7051 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+     --peerAddresses localhost:7060 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" \
+     --peerAddresses localhost:7057 --tlsRootCertFiles "../organizations/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt" \
+     --tls --cafile $ORDERER_GENERAL_TLS_ROOTCAS
